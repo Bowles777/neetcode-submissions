67dbb919417da6b06e2d921beaae85b2f38002cc
Luke Bowles <luke.bowles7@gmail.com>
2026-05-13 16:14:31 +0100

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        //create map to store annograms
+        const anagrams = new Map();
+
+        //Sort through original array
+        strs.map((i) => {
+            if (i.length === 0) {
+                console.log("here")
+                const non = anagrams.get("");
+                if (non) {
+                    non.push("")
+                    anagrams.set("", non);
+                } else {
+                    anagrams.set("", [""]);
+                }
+            } else {
+                const sorted = i.split("").sort().join("");
+                // console.log(sorted);
+                const arr = anagrams.get(sorted);
+                if (arr) {
+                    arr.push(i)
+                    anagrams.set(sorted, arr);
+                } else {
+                    anagrams.set(sorted, [i]);
+                }
+            }
+        });
+        const result = [...anagrams].map(([key, val]) => val);
+        return result;
+        // return null;
+    }
+}
